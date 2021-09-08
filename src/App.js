7cfa15533a4e795651e60bbb3f74cba640bdc27a
David Kamau <dkamau716@gmail.com>
2021-09-08 18:40:53 +0300

    Adding project files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+//We use the below statement to link css to our html files
+import './App.css';
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <h1>This is where html structures will be placed</h1>  
+      </div>
+    )
+  }
+}
+
+export default App;
